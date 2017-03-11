1320870b69fdce07fd77f46378ae4439be20e81b
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-03-11 15:51:22 +0100

    Generalize wineview into a shopview and specific wine operations
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ public/js/views/shopview.js
@@ -0,0 +1,124 @@
+/**
+ * Winzerhof Wurst – www.winzerhof-wurst.at
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ * @copyright Christoph Wurst 2016-2017
+ */
+
+define(function (require) {
+	'use strict';
+
+	var Handlebars = require('handlebars');
+	var Marionette = require('marionette');
+
+	var winesTemplate = require('text!templates/wines.html');
+	var radio = require('radio');
+
+	/**
+	 * @class WinesView
+	 */
+	var WinesView = Marionette.LayoutView.extend({
+
+		template: Handlebars.compile(winesTemplate),
+
+		ui: {
+			firstname: '#firstname',
+			lastname: '#lastname',
+			street: '#street',
+			nr: '#nr',
+			zipcode: '#zipcode',
+			city: '#city',
+			telephone: '#telephone',
+			fax: '#fax',
+			email: '#email',
+			comment: '#comment',
+			submit: '#submit-wines',
+			successAlert: '#alert-success',
+			errorAlert: '#alert-error'
+		},
+
+		events: {
+			'click @ui.submit': '_onSubmit'
+		},
+
+		regions: {
+			listWhite: '#white-wine-list',
+			listRed: '#red-wine-list',
+			listSpecialQuality: '#special-quality-wine-list',
+			list1l: '#wine-1l-list'
+		},
+
+		/**
+		 * @protected
+		 * @returns {undefined}
+		 */
+		_loadData: function () {
+			throw new Error('not implemented');
+		},
+
+		/**
+		 * @returns {undefined}
+		 */
+		onShow: function () {
+			this._loadData();
+		},
+
+		/**
+		 * @returns {undefined}
+		 */
+		_resetForm: function () {
+			// Can be overridden
+		},
+
+		/**
+		 * @protected
+		 * @argument {Object} data
+		 * @returns {undefined}
+		 */
+		_onBeforeSubmit: function (data) {
+			// Can be overridden
+		},
+
+		/**
+		 * @returns {undefined}
+		 */
+		_onSubmit: function () {
+			var data = {
+				firstname: this.ui.firstname.val(),
+				lastname: this.ui.lastname.val(),
+				street: this.ui.street.val(),
+				nr: this.ui.nr.val(),
+				zipcode: this.ui.zipcode.val(),
+				city: this.ui.city.val(),
+				email: this.ui.email.val(),
+				comment: this.ui.comment.val()
+			};
+			this._onBeforeSubmit(data);
+
+			this.$('input,textarea,.add-6,.add-12').prop('disabled', true);
+			this.ui.submit.button('loading');
+			this.ui.successAlert.hide();
+			this.ui.errorAlert.hide();
+
+			var saving = radio.cart.request('submit:wines', data);
+
+			saving.done(function () {
+				this._resetForm();
+				this.ui.successAlert.show();
+			}.bind(this));
+			saving.fail(function () {
+				this.ui.errorAlert.show();
+			}.bind(this));
+			saving.always(function () {
+				this.$('input,textarea,.add-6,.add-12').prop('disabled', false);
+				this.ui.submit.button('reset');
+			}.bind(this));
+		}
+
+	});
+
+	return WinesView;
+});
--- public/js/views/winesview.js
+++ public/js/views/winesview.js
@@ -5,54 +5,37 @@
  * later. See the COPYING file.
  *
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
- * @copyright Christoph Wurst 2016-2017
+ * @copyright Christoph Wurst 2017
  */
 
 define(function (require) {
 	'use strict';
 
 	var $ = require('jquery');
-	var Handlebars = require('handlebars');
-	var Marionette = require('marionette');
 
-	var winesTemplate = require('text!templates/wines.html');
-	var radio = require('radio');
+	var ShopView = require('views/shopview');
 	var WineList = require('views/winelist');
+	var radio = require('radio');
 
 	/**
 	 * @class WinesView
 	 */
-	var WinesView = Marionette.LayoutView.extend({
-		template: Handlebars.compile(winesTemplate),
-		ui: {
-			firstname: '#firstname',
-			lastname: '#lastname',
-			street: '#street',
-			nr: '#nr',
-			zipcode: '#zipcode',
-			city: '#city',
-			telephone: '#telephone',
-			fax: '#fax',
-			email: '#email',
-			comment: '#comment',
-			submit: '#submit-wines',
-			successAlert: '#alert-success',
-			errorAlert: '#alert-error'
-		},
-		events: {
-			'click @ui.submit': '_onSubmit'
-		},
-		regions: {
-			listWhite: '#white-wine-list',
-			listRed: '#red-wine-list',
-			listSpecialQuality: '#special-quality-wine-list',
-			list1l: '#wine-1l-list'
-		},
-		onShow: function () {
+	var WinesView = ShopView.extend({
+
+		/**
+		 * @type {WineCollection}
+		 */
+		_wines: undefined,
+
+		/**
+		 * @override
+		 * @returns {undefined}
+		 */
+		_loadData: function () {
 			var loadingWines = radio.wine.request('entities');
 
 			$.when(loadingWines).done(function (wines) {
-				this.wines = wines;
+				this._wines = wines;
 				this.listWhite.show(new WineList({
 					collection: wines,
 					filter: function (wine) {
@@ -79,37 +62,24 @@ define(function (require) {
 				}));
 			}.bind(this));
 		},
-		_onSubmit: function () {
-			var data = {
-				firstname: this.ui.firstname.val(),
-				lastname: this.ui.lastname.val(),
-				street: this.ui.street.val(),
-				nr: this.ui.nr.val(),
-				zipcode: this.ui.zipcode.val(),
-				city: this.ui.city.val(),
-				email: this.ui.email.val(),
-				comment: this.ui.comment.val(),
-				wines: this.wines.toJSON()
-			};
-			this.$('input,textarea,.add-6,.add-12').prop('disabled', true);
-			this.ui.submit.button('loading');
-			this.ui.successAlert.hide();
-			this.ui.errorAlert.hide();
 
-			var saving = radio.cart.request('submit:wines', data);
+		/**
+		 * @override
+		 * @param {Object} data
+		 * @returns {undefined}
+		 */
+		_onBeforeSubmit: function (data) {
+			data.wines = this._wines.toJSON();
+		},
 
-			saving.done(function () {
-				this.wines.trigger('clear');
-				this.ui.successAlert.show();
-			}.bind(this));
-			saving.fail(function () {
-				this.ui.errorAlert.show();
-			}.bind(this));
-			saving.always(function () {
-				this.$('input,textarea,.add-6,.add-12').prop('disabled', false);
-				this.ui.submit.button('reset');
-			}.bind(this));
+		/**
+		 * @override
+		 * @returns {undefined}
+		 */
+		_resetForm: function () {
+			this._wines.trigger('clear');
 		}
+
 	});
 
 	return WinesView;
