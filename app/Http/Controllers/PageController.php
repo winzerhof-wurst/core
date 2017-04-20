08a589765578fbade83fec6836e94431a92089d8
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-04-20 10:01:26 +0200

    Remove debuggin exception
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
 use function view;
 
 class PageController extends Controller {
 
 	public function index() {
-		throw new Exception();
 		return view('home');
 	}
 
