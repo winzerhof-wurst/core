22b6b21df2b5becb714c7d7c30aa13912aab3ba5
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-03-17 17:45:16 +0100

    Add tel and emailto tracking
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- assets/js/views/contactview.js
+++ assets/js/views/contactview.js
@@ -5,10 +5,10 @@
  * later. See the COPYING file.
  *
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
- * @copyright Christoph Wurst 2016
+ * @copyright Christoph Wurst 2018
  */
 
-define(function(require) {
+define(function (require) {
 	'use strict';
 
 	var Marionette = require('backbone.marionette');
@@ -19,7 +19,20 @@ define(function(require) {
 	 * @class ContactView
 	 */
 	var ContactView = Marionette.ItemView.extend({
-		template: AboutTemplate
+		template: AboutTemplate,
+
+		events: {
+			'click a[href^="tel:"]': '_onClickTel',
+			'click a[href^="mailto:"]': '_onClickEmail'
+		},
+
+		_onClickTel: function () {
+			_paq.push(['trackEvent', 'contactform', 'called', this.href]);
+		},
+
+		_onClickEmail: function () {
+			_paq.push(['trackEvent', 'contactform', 'mail sent', this.href]);
+		}
 	});
 
 	return ContactView;
