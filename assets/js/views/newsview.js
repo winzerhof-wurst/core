e5ae452bb8d323dcbf44584d9d93c16dace43a98
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-03-28 11:47:32 +0200

    Make market section dynamic and split news page into partials
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- assets/js/views/newsview.js
+++ assets/js/views/newsview.js
@@ -5,10 +5,10 @@
  * later. See the COPYING file.
  *
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
- * @copyright Christoph Wurst 2016
+ * @copyright Christoph Wurst 2018
  */
 
-define(function(require) {
+define(function (require) {
 	'use strict';
 
 	var Marionette = require('backbone.marionette');
@@ -19,7 +19,32 @@ define(function(require) {
 	 * @class NewsView
 	 */
 	var NewsView = Marionette.CompositeView.extend({
-		template: NewsTemplate
+		template: NewsTemplate,
+
+		templateHelpers: function () {
+			var marketDates = [
+				new Date(2018, 0, 20),
+				new Date(2018, 1, 17),
+				new Date(2018, 2, 17),
+				new Date(2018, 3, 21),
+				new Date(2018, 4, 19),
+				new Date(2018, 5, 16),
+				new Date(2018, 6, 21),
+				new Date(2018, 7, 18),
+				new Date(2018, 8, 15),
+				new Date(2018, 9, 20),
+				new Date(2018, 10, 17),
+				new Date(2018, 11, 15)
+			];
+			var today = new Date();
+
+			return {
+				marketDates: marketDates.filter(function (date) {
+					return date.getFullYear() >= today.getFullYear()
+						&& date.getMonth() >= today.getMonth();
+				})
+			};
+		}
 	});
 
 	return NewsView;
