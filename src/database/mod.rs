5b36101f6993da7428f2967e84be5ee1ae8dc1d8
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-12-20 22:14:59 +0100

    Move to async syntax
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,2 +1,9 @@
 pub mod models;
 pub mod schema;
+
+use diesel::pg::PgConnection;
+use diesel::r2d2;
+
+pub type Connection = PgConnection;
+pub type ConnectionManager = r2d2::ConnectionManager<Connection>;
+pub type Pool = r2d2::Pool<ConnectionManager>;
--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,58 @@
-extern crate actix;
-extern crate actix_web;
-extern crate chrono;
 #[macro_use]
 extern crate diesel;
-extern crate dotenv;
-extern crate failure;
-extern crate futures;
-extern crate http;
-extern crate listenfd;
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
 use std::env;
+use std::io;
 
-use actix::prelude::*;
-use actix_web::{server, App, AsyncResponder, HttpMessage, HttpRequest, HttpResponse, Json};
-use diesel::pg::PgConnection;
-use diesel::prelude::*;
+use actix_web::{web, App, Error as AWError, HttpResponse, HttpServer};
 use dotenv::dotenv;
 use failure::Error;
-use futures::prelude::*;
-use http::Method;
-use listenfd::ListenFd;
 
 mod database;
 mod services;
 
-use services::shop::{FetchTidbits, FetchWines, PostOrder, WinesActor};
-
-pub fn establish_db_connection() -> PgConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+async fn fetch_tidbits(db: web::Data<database::Pool>) -> Result<HttpResponse, AWError> {
+    match services::shop::fetch_tidbits(&db.get().unwrap()) {
+        Ok(tidbits) => Ok(HttpResponse::Ok().json(tidbits)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    }
 }
 
-fn fetch_tidbits(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    req.state()
-        .wines
-        .send(FetchTidbits {})
-        .from_err()
-        .and_then(|res| match res {
-                      Ok(user) => Ok(HttpResponse::Ok().json(user)),
-                      Err(_) => Ok(HttpResponse::InternalServerError().into()),
-                  })
-        .responder()
+async fn fetch_wines(db: web::Data<database::Pool>) -> Result<HttpResponse, Error> {
+    match services::shop::fetch_wines(&db.get().unwrap()) {
+        Ok(wines) => Ok(HttpResponse::Ok().json(wines)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    }
 }
 
-fn fetch_wines(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    req.state()
-        .wines
-        .send(FetchWines {})
-        .from_err()
-        .and_then(|res| match res {
-                      Ok(user) => Ok(HttpResponse::Ok().json(user)),
-                      Err(_) => Ok(HttpResponse::InternalServerError().into()),
-                  })
-        .responder()
+async fn post_order(
+    order: web::Json<services::shop::Order>,
+    db: web::Data<database::Pool>,
+) -> Result<HttpResponse, Error> {
+    match services::shop::post_order(order.into_inner(), &db.get().unwrap()) {
+        Ok(wines) => Ok(HttpResponse::Ok().json(wines)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    }
 }
 
-#[derive(Deserialize)]
-struct Order {
-    firstname: String,
-    lastname: String,
-    street: String,
-    nr: String,
-    zipcode: usize,
-    city: String,
-    email: String,
-    telephone: String,
-    fax: String,
-    comment: Option<String>,
-    tidbit_ids: Option<Vec<i32>>,
-    wine_ids: Option<Vec<i32>>,
-}
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().unwrap();
+    let conn_mgr = database::ConnectionManager::new(
+        env::var("DATABASE_URL").expect("DATABASE_URL is missing"),
+    );
+    let pool = database::Pool::new(conn_mgr).unwrap();
 
-fn post_order(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    let req = req.clone();
-    req.json()
-        .from_err()
-        .and_then(move |order: Order| {
-            req.state()
-                .wines
-                .send(PostOrder::new(order.firstname.to_owned(),
-                                     order.lastname.to_owned(),
-                                     order.street.to_owned(),
-                                     order.nr.to_owned(),
-                                     order.zipcode.to_owned(),
-                                     order.city.to_owned(),
-                                     order.email.to_owned(),
-                                     order.telephone.to_owned(),
-                                     order.fax.to_owned(),
-                                     order.comment.to_owned(),
-                                     order
-                                         .tidbit_ids
-                                         .clone()
-                                         .map(|ids| ids.clone())
-                                         .unwrap_or(vec![]),
-                                     order
-                                         .wine_ids
-                                         .clone()
-                                         .map(|ids| ids.clone())
-                                         .unwrap_or(vec![])))
-                .from_err()
-        })
-        .and_then(|res| match res {
-                      Ok(user) => Ok(HttpResponse::Ok().json(user)),
-                      Err(_) => Ok(HttpResponse::InternalServerError().into()),
-                  })
-        .responder()
-}
-
-struct State {
-    wines: Addr<WinesActor>,
-}
-
-fn main() {
-    let mut listenfd = ListenFd::from_env();
-    let mut server = server::new(|| {
-        let addr = SyncArbiter::start(3, || WinesActor::new(establish_db_connection()));
-
-        App::with_state(State { wines: addr.clone() })
-            .resource("/tidbits", |r| r.method(Method::GET).a(fetch_tidbits))
-            .resource("/wines", |r| r.method(Method::GET).a(fetch_wines))
-            .resource("/api/orders", |r| r.method(Method::POST).a(post_order))
-    });
-
-    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
-        server.listen(l)
-    } else {
-        server.bind("127.0.0.1:3000").unwrap()
-    };
-
-    server.run();
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .service(web::resource("/tidbits").route(web::get().to(fetch_tidbits)))
+            .service(web::resource("/wines").route(web::get().to(fetch_wines)))
+            .service(web::resource("/api/orders").route(web::post().to(post_order)))
+    })
+    .bind("127.0.0.1:3000")?
+    .start()
+    .await
 }
