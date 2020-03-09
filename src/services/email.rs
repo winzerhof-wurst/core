9a21e7378ec7d73b2b9b2634bfcf83b14f830f9e
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-03-09 19:51:15 +0100

    Fix SMTP connection
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/services/email.rs
+++ src/services/email.rs
@@ -1,15 +1,26 @@
 use std::env;
 
 use failure::Error;
-use lettre::smtp::{authentication::Credentials, SmtpTransport};
+use lettre::smtp::client::net::ClientTlsParameters;
+use lettre::smtp::{authentication, ClientSecurity, SmtpTransport};
 use lettre::SmtpClient;
+use native_tls::{Protocol, TlsConnector};
 
 pub fn get_mailer() -> Result<SmtpTransport, Error> {
+    let domain = "smtp.world4you.com";
     let user = env::var("MAIL_USER")?;
     let pwd = env::var("MAIL_PASSWORD")?;
 
-    let creds = Credentials::new(user, pwd);
-    Ok(SmtpClient::new_simple("smtp.world4you.com")?
-        .credentials(creds)
-        .transport())
+    let creds = authentication::Credentials::new(user, pwd);
+
+    let mut tls_builder = TlsConnector::builder();
+    tls_builder.min_protocol_version(Some(Protocol::Tlsv12));
+
+    let tls_parameters = ClientTlsParameters::new(domain.to_string(), tls_builder.build()?);
+    Ok(
+        SmtpClient::new((domain, 587), ClientSecurity::Required(tls_parameters))?
+            .authentication_mechanism(authentication::Mechanism::Login)
+            .credentials(creds)
+            .transport(),
+    )
 }
